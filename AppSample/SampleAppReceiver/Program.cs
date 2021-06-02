a48c0387fe11a79932e3742d4f943b664e4181de
Diego Cantelli <diegocantelli19@gmail.com>
2021-06-02 19:43:21 -0300

    Criação inicial dos projetos

---
+++ AppSample/SampleAppReceiver/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SampleAppReceiver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
